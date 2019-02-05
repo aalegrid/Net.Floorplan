e02c19da031ed7b1d938238fd894d9f50a9eabed
Alex Alegrid <alex@alegrid.com>
2019-02-05 19:40:34 +1100

    Latest

--- FP/Scripts/site.js
+++ FP/Scripts/site.js
@@ -659,7 +659,45 @@ var floorplanHistory = {
 
         });
 
+        // Wrap IIFE around your code
+        (function ($, viewport) {
+            $(document).ready(function () {
 
+                
+
+                // Executes only in XS breakpoint
+                if (viewport.is('xs')) {
+                    // ...
+                    
+                   // $('.fp-image-view, .right-panel').css("height", height + "px");
+                }
+
+                // Executes in SM, MD and LG breakpoints
+                if (viewport.is('>=sm')) {
+                    // ...
+                }
+
+                // Executes in XS and SM breakpoints
+                if (viewport.is('<md')) {
+                    // ...
+                    
+                }
+
+                // Execute code each time window size changes
+                $(window).resize(
+                    viewport.changed(function () {
+                        //if (viewport.is('xs')) {
+                           
+                        //}
+                        var height = $('#floorplan').height() + 35;
+                        $('.fp-image-view, .right-panel').css("height", height + "px");
+                    })
+                );
+
+                var height = $('#floorplan').height() + 35;
+                $('.fp-image-view, .right-panel').css("height", height + "px");
+            });
+        })(jQuery, ResponsiveBootstrapToolkit);
 
     },
 
